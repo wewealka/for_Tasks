8f5426eb54128f120caf60ad5085f097e48a53ec
wewealka228 <wewepoweralka@mail.ru>
2024-10-18 02:09:06 +0300

    add 2 mainBtns for state changing

--- src/tasks/taskForMap/accordion/singleAccordion/SingleAccordion.tsx
+++ src/tasks/taskForMap/accordion/singleAccordion/SingleAccordion.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+import { MainButton } from "../../../../components/Button";
+
 import { AccordionBodyType, AccordionPropsType } from "../../../../data/AccordionData";
 
 export function SingleAccordion(props: AccordionPropsType) {
+
+    let [stateClps, setStateClps] = useState(props.accordionBody)
+    const clpsHandlerOn = () => {
+        setStateClps([])
+    }
+    const clpsHandlerOff = () => {
+        setStateClps(props.accordionBody)
+    }
     return (
         <div>
             <h3>{props.title}</h3>
-            {!props.collapsed && (
-                <ul >
-                {props.accordionBody.map((AccordionBody: AccordionBodyType) => (
-                    <li key={AccordionBody.id}>{AccordionBody.content}</li>
+            <ul >
+                <MainButton name={"covered"} callBack={clpsHandlerOn} />
+                <MainButton name={"Uncovered"} callBack={clpsHandlerOff} />
+                {stateClps.map((AccordionBody: AccordionBodyType) => (
+                    <li key={AccordionBody.id}>
+                        {AccordionBody.content}
+                    </li>
                 ))}
             </ul>
-            )}
         </div>
     )
 }
