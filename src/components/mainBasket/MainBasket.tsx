af9f215d512cbc8b8a35352f238c00b7f2b1b403
wewealka228 <wewepoweralka@mail.ru>
2024-10-19 00:53:20 +0300

    add rating

--- src/components/mainBasket/MainBasket.tsx
+++ src/components/mainBasket/MainBasket.tsx
@@ -7,7 +7,6 @@ import { Accordion } from "../../tasks/taskForMap/accordion/Accordion"
 import { AppTitle } from "../../tasks/taskForMap/AppTitle"
 import { TaskForTrainingUseState } from "../../tasks/TaskForUseState/TaskUSeState"
 import { MainButton } from "../Button"
-
 import { Delimeter } from "../stupidComponents/Delimeter"
 import { Rating } from "../../tasks/taskForConditional-Rendering/Raiting"
 import OnOff from "../../tasks/TaskForUseState/OnOff"
@@ -15,32 +14,42 @@ import OnOff from "../../tasks/TaskForUseState/OnOff"
 export const MainBasket = () => {
     return (
         <div className="MainBasket">
-            <OnOff/>
-            <OnOff/>
-            <OnOff/>
-            <OnOff/>
+            <OnOff />
+            <OnOff />
+            <OnOff />
+            <OnOff />
             <Delimeter /> {/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
-            <TaskForInput/>
+
+            <TaskForInput />
+
             <Delimeter /> {/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <FilterMethod />
+
             <Delimeter /> {/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <TaskForTrainingUseState />
+
             <Delimeter />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <MainButton name={"MyYouTubeChanel-1"} callBack={() => dataButtonfoo1('viva', 21, 'Minsk')} />
             <MainButton name={"MyYouTubeChanel-2"} callBack={() => dataButtonfoo2('vova')} />
             <MainButton name={"3foo"} callBack={dataButtonfoo3} />
+
             <Delimeter />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <AppTitle titles={dataAppTitle} />
+
             <Delimeter />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <Accordion />
+
             <Delimeter />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+
+            <Rating />
+
             <Delimeter />{/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
             <Task1 cars={topCars} />
         </div>
     )
